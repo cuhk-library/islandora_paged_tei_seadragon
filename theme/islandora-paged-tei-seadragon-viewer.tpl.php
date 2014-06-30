c1d7d77bc313729dee61150913b89573877c2026
willtp87 <willtp87@hotmail.com>
2014-06-30 20:20:11 +0000

    interim

---
+++ theme/islandora-paged-tei-seadragon-viewer.tpl.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @file
+ * Template file for the paged TEI seadragon viewer.
+ */
+?>
+<div class="viewer-tei-pane">
+<?php print $tei;?>
+</div>
+<div class="viewer-image-pane">
+<?php print $seadragon;?>
+</div>
