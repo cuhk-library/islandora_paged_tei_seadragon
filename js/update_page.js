5f6219ce9e09941469f899b8d62b74150ce548bd
willtp87 <willtp87@hotmail.com>
2014-07-04 17:13:43 +0000

    ajaxing page changes now using tokens

--- js/update_page.js
+++ js/update_page.js
@@ -2,17 +2,24 @@
     Drupal.behaviors.islandora_paged_tei_seadragon_update_page = {
         attach: function (context, settings) {
             var set_page = function (pid) {
-                token = "";
-                settings.islandoraOpenSeadragon.resourceUri = "http://dgi-vagrant-i7latest.local/islandora/object/"
-                    + pid + "/datastream/JP2/view" + token;
-                // Updating the PID to keep it consistent, it isn't used.
-                settings.islandoraOpenSeadragon.pid = pid;
-                tile_source = new OpenSeadragon.DjatokaTileSource(
-                    settings.islandoraOpenSeadragon.settings.djatokaServerBaseURL,
-                    settings.islandoraOpenSeadragon.resourceUri,
-                    settings.islandoraOpenSeadragon
-                );
-                Drupal.settings.islandora_open_seadragon_viewer.open(tile_source);
+                $.ajax({
+                    url: Drupal.settings.basePath + "islandora/rest/v1/object/" + pid + "/datastream/JP2/token",
+                    cache: false,
+                    success: function(token) {
+                        settings.islandoraOpenSeadragon.resourceUri =
+                            location.protocol + "//" + location.host + "/" +
+                            Drupal.settings.basePath + "islandora/object/" + pid
+                            + "/datastream/JP2/view?token=" + token;
+                        // Updating the PID to keep it consistent, it isn't used.
+                        settings.islandoraOpenSeadragon.pid = pid;
+                        tile_source = new OpenSeadragon.DjatokaTileSource(
+                            settings.islandoraOpenSeadragon.settings.djatokaServerBaseURL,
+                            settings.islandoraOpenSeadragon.resourceUri,
+                            settings.islandoraOpenSeadragon
+                        );
+                        Drupal.settings.islandora_open_seadragon_viewer.open(tile_source);
+                    }
+                })
             };
             $("#islandora_paged_tei_seadragon_pager").change(function () {
                 set_page($(this).val());
