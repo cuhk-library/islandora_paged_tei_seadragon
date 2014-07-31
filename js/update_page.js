ea12b33294ab223e2a9e11e9493ad9f2bf47a6da
Jordan Dukart <jordan@discoverygarden.ca>
2014-07-31 17:31:41 +0000

    Grab the DSID from the scope.

--- js/update_page.js
+++ js/update_page.js
@@ -91,7 +91,8 @@
                                     }
                                     download = "<div>" + Drupal.settings.islandora_paged_tei_seadragon.download_prefix
                                         + "<a href=" + Drupal.settings.basePath + "islandora/object/"
-                                    + pid + "/datastream/" + dsid + "/download" + ">" + datastream_info.dsid + " (" + size + ")" + "</a></div>";
+                                    + pid + "/datastream/" + datastream_info.dsid + "/download" + ">" + datastream_info.dsid + " (" + size + ")" + "</a></div>";
+                                    console.log(download);
                                     $(".paged-tei-seadragon-viewer-download-datastreams").append(download);
                                 }
                             });
