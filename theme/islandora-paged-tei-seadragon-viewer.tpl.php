f8a7b52817b3367cd7cb3ce4076a3bee5fadbf7d
Jordan Dukart <jordan@discoverygarden.ca>
2014-07-31 17:17:07 +0000

    Only render up what's present.

--- theme/islandora-paged-tei-seadragon-viewer.tpl.php
+++ theme/islandora-paged-tei-seadragon-viewer.tpl.php
@@ -4,31 +4,37 @@
  * Template file for the paged TEI seadragon viewer.
  */
 ?>
-<div class="paged-tei-seadragon-viewer-pane" id="paged-tei-seadragon-viewer-tei-pane">
-  <div>
-    <div class="left-toolbar">
-      <?php if (isset($seadragon)): ?>
-        <button type="button" id="paged-tei-seadragon-viewer-tei-toggle">Toggle TEI</button>
-      <?php endif; ?>
+<?php if ($tei_pages_populated): ?>
+  <?php if ($tei): ?>
+  <div class="paged-tei-seadragon-viewer-pane" id="paged-tei-seadragon-viewer-tei-pane">
+    <div>
+      <div class="left-toolbar">
+        <?php if (isset($seadragon)): ?>
+          <button type="button" id="paged-tei-seadragon-viewer-tei-toggle">Toggle TEI</button>
+        <?php endif; ?>
+      </div>
+    </div>
+    <div id="paged-tei-seadragon-viewer-tei">
+      <?php print $tei;?>
     </div>
   </div>
-  <div id="paged-tei-seadragon-viewer-tei">
-    <?php print $tei;?>
-  </div>
-</div>
-<div class="paged-tei-seadragon-viewer-pane" id="paged-tei-seadragon-viewer-seadragon-pane">
-  <div>
-    <div class="paged-tei-seadragon-viewer-left-toolbar">
-      <?php print isset($pager) ? $pager : '';?>
+  <?php endif; ?>
+  <div class="paged-tei-seadragon-viewer-pane" id="paged-tei-seadragon-viewer-seadragon-pane">
+    <div>
+      <div class="paged-tei-seadragon-viewer-left-toolbar">
+        <?php print isset($pager) ? $pager : '';?>
+      </div>
+      <div class="paged-tei-seadragon-viewer-right-toolbar">
+        <?php print isset($clipper) ? $clipper : '';?>
+      </div>
     </div>
-    <div class="paged-tei-seadragon-viewer-right-toolbar">
-      <?php print isset($clipper) ? $clipper : '';?>
+    <div id="paged-tei-seadragon-viewer-seadragon">
+      <?php print isset($seadragon) ? $seadragon : '';?>
     </div>
   </div>
-  <div id="paged-tei-seadragon-viewer-seadragon">
-    <?php print isset($seadragon) ? $seadragon : '';?>
+  <div class="paged-tei-seadragon-viewer-download-datastreams">
+    <?php print isset($datastreams) ? $datastreams : '';?>
   </div>
-</div>
-<div class="paged-tei-seadragon-viewer-download-datastreams">
-  <?php print isset($datastreams) ? $datastreams : '';?>
-</div>
+<?php else: ?>
+  <p><?php print t('This manuscript currently does not contain any page image(s) or text.'); ?></p>
+<?php endif; ?>
